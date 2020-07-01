eb6e469d7aeda3508cfe94f3ead4c56a969fd02d
Iven <iwenchak@gmail.com>
2020-07-01 23:43:28 +0300

    created models and interfaces

---
+++ CoreMVC/CoreMVC/Data/Mocks/MockCategory.cs
@@ -0,0 +1,27 @@
+﻿using CoreMVC.Data.Interfaces;
+using CoreMVC.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Data.Mocks
+{
+	public class MockCategory : ICarsCategory
+	{
+		public IEnumerable<Category> AllCategories 
+			
+		{
+			get 
+			{
+			return new List<Category>
+			{
+				new Category { CategoryName = "Electric car", Desc = "Modern transport" },
+				new Category { CategoryName = "Car with internal combustion engine", Desc = "Classic car" }
+			};
+			}
+
+		}
+		
+	}
+}
