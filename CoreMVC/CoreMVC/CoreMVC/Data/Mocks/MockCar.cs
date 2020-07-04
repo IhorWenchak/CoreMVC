d37e9fe3357976dd0b7329350d530f8ff13ec661
Iven <iwenchak@gmail.com>
2020-07-04 11:10:11 +0300

    change in bootstrap

--- CoreMVC/CoreMVC/CoreMVC/Data/Mocks/MockCar.cs
+++ CoreMVC/CoreMVC/CoreMVC/Data/Mocks/MockCar.cs
@@ -18,8 +18,8 @@ namespace CoreMVC.Data.Mocks
 				return new List<Car> {
 					new Car {
 						Name = "Tesla Model S",
-						ShortDesc = "Fast car",
-						LongDesc = "Beautiful, fast and very quiet car",
+						ShortDesc = "Fast car.",
+						LongDesc = "Beautiful, fast and very quiet car.",
 						Img = "/img/1024px-Tesla_Model_S_02_2013.jpg",
 						Price = 45000,
 						IsFavourite = true,
@@ -27,8 +27,8 @@ namespace CoreMVC.Data.Mocks
 					},
 					new Car {
 						Name = "Nissan Leaf",
-						ShortDesc = "Quiet and economical",
-						LongDesc = "Convenient car for the city",
+						ShortDesc = "Quiet and economical.",
+						LongDesc = "Convenient car for the city.",
 						Img = "/img/nissan-leaf-jdm-2020.jpg",
 						Price = 15000, IsFavourite = true,
 						Available = true,
@@ -36,8 +36,8 @@ namespace CoreMVC.Data.Mocks
 					},
 					new Car {
 						Name = "BMW M3",
-						ShortDesc = "Daring and stylish",
-						LongDesc = "Convenient car for the city",
+						ShortDesc = "Daring and stylish.",
+						LongDesc = "Convenient car for the city.",
 						Img = "/img/bmw-m3-sedan-render.jpg",
 						Price = 65000, IsFavourite = true,
 						Available = true,
@@ -45,8 +45,8 @@ namespace CoreMVC.Data.Mocks
 					},
 					new Car {
 						Name = "Mercedes C class",
-						ShortDesc = "Cozy and large",
-						LongDesc = "Convenient car for the city",
+						ShortDesc = "Cozy and large.",
+						LongDesc = "Convenient car for the city.",
 						Img = "/img/1400x936.jpg",
 						Price = 40000, IsFavourite = false,
 						Available = false,
